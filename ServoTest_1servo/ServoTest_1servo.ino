0d04ac6ec4682bc9edd5d73907f3a5a35bf1312f
unknown <kyshnd.ubuntu@gmail.com>
2015-08-24 18:51:14 +0900

    add sample file

---
+++ ServoTest_1servo/ServoTest_1servo.ino
@@ -0,0 +1,24 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+// This example code is in the public domain.
+
+
+#include <Servo.h> 
+ 
+Servo myservo1;  // create servo object to control a servo 
+ 
+int pos = 0;    // variable to store the servo position 
+int pos2 = 180; 
+void setup() 
+{ 
+  myservo1.attach(3);  // attaches the servo on pin 3 to the servo object 
+} 
+ 
+ 
+void loop() 
+{ 
+  myservo1.write(pos2);
+  delay(2000);
+  myservo1.write(pos2/2);
+  delay(2000);
+} 
