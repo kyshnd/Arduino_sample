12786b84ad86a22c25e66c8cd614a79ba87e61d9
unknown <kyshnd.ubuntu@gmail.com>
2015-08-26 15:34:53 +0900

    change code by Otomori

--- ServoTest_1servo/ServoTest_1servo.ino
+++ ServoTest_1servo/ServoTest_1servo.ino
@@ -2,23 +2,39 @@
 // by BARRAGAN <http://barraganstudio.com> 
 // This example code is in the public domain.
 
-
 #include <Servo.h> 
  
-Servo myservo1;  // create servo object to control a servo 
+Servo left;  // create servo object to control a servo 
+Servo right;  // create servo object to control a servo 
  
-int pos = 0;    // variable to store the servo position 
-int pos2 = 180; 
-void setup() 
+//int pos = 0;    // variable to store the servo position 
+//int pos2 = 180; 
+
+int rau = 90;
+int rad = 0;
+int lau = 90;
+int lad = 180;
+
+void setup()
 { 
-  myservo1.attach(3);  // attaches the servo on pin 3 to the servo object 
+  left.attach(3);  // attaches the servo on pin 3 to the servo object 
+  right.attach(5);  // attaches the servo on pin 5 to the servo object 
 } 
- 
- 
+
 void loop() 
-{ 
-  myservo1.write(pos2);
+{
+//  left.write(pos2);
+//  right.write(pos);
+//  delay(2000);
+//  left.write(pos2/2);
+//  right.write(pos2/2);
+//  delay(2000);
+
+  left.write(rau);
+  right.write(lau);
   delay(2000);
-  myservo1.write(pos2/2);
+  right.write(rad);
   delay(2000);
-} 
+  left.write(lad);
+  delay(2000);
+}
